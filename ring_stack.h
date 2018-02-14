f1b4738862fcc53bf9a5f44277aaa514c93b901b
Alex Koch <30006902+Alex-Koch-2200@users.noreply.github.com>
2018-02-14 10:51:20 +0000

    Update ring_stack.h

--- ring_stack.h
+++ ring_stack.h
@@ -1,6 +1,7 @@
 /*
-    ring_stack
-    Alex Koch 2018
+    ring_stack.h
+    MANSEDS Self-Landing Rocket code
+    Contributors: Alex Koch
 
     Last-in-first-out ring stack
 
@@ -18,29 +19,28 @@ private:
     T * container_;
     uint32_t top_, shift_ increment_;
 public:
-    ring_stack(unsigned bits) : shift_{ uint32_t(32 - bits) }, 
+    ring_stack(unsigned bits) : top_{ 0 }, shift_{ uint32_t(32 - bits) }, 
                                 increment_{ uint32_t( pow(2, 32) / pow(2, bits) ) }, 
                                 container_{ new T[ uint32_t( pow(2, bits) ) ] } 
                                 { 
-                                    top_ = increment_; // offset top to 1
-                                    container_[0] = nullptr; // set the first element to nullptr so that it can be checked if the array has been filled yet
+                                    container_[0] = nullptr; // set the first element to nullptr so that it can be checked if the array has been filled
                                 }
-    bool empty() { return container_[0] == nullptr && top_ == 1; }
+    bool empty() { return container_[0] == nullptr && top_ == 0; }
     bool full() { return container_[0] != nullptr; } // Container is filled only once the 0th element has been overwritten via a push() call
     uint16_t size() 
     { 
         if(container_[0] == nullptr)
         {
-            return top_ >> shift_;
+            return top_ >> shift_; // The shifted value of top_ corresponds to the number of elements in the stack
         }
-        return max_size;
+        else{ return max_size; } // The stack has been filled and so the number of elements is always the max size as any new ones will overwrite the oldest element.
     }
     uint32_t max_size() { return pow(32 - shift_, 2); }
     T & top() { return container_[top_ >> increment_]; }
     void push(const T element) 
-    {
+    {   
+        top_ += increment_; // Must shift the top first so that it corresponds to the slot in which to place the new element
         container_[top_ >> shift_] = element;
-        top_ += increment_;
     }
     // emplace?
     void pop() 
